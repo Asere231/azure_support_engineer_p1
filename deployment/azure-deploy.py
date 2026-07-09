1389650c04b586dcfba77c2f480b3498ff513324
Anonemous2 <AndrewFBates@icloud.com>
2026-07-09 12:44:55 -0400

    Added template py script and deployment folder.

---
+++ deployment/azure-deploy.py
@@ -0,0 +1,25 @@
+# Sample deployment script.
+
+# Azure development deployment configs.
+rg_name     = 'rg-p1-dev'
+rg_location = 'canadaeast'
+
+# Create RG for project 1.
+# TODO: Check for RG
+
+# TODO: Create RG if not found, with correct params.
+# TODO: When creating RG, add correct members and roles.
+
+# Create Virtual Network.
+# TODO: Setup with correct subnets, security groups, routes, etc.
+
+# Create VMs
+# Create JWT authorization server.
+# TODO: Create VM, send bootsrapping files and spin-up FASTAPI docker container.
+
+# Create private endpoint server.
+# TODO:
+
+# Setup Azure resource monitoring.
+# TODO:
+
